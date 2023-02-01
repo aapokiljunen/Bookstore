809ecc1657d9829df4520306229e8e85a9ef917f
Aapo <aapo.kiljunen@gmail.com>
2023-02-01 12:52:12 +0200

    projekti luotu

---
+++ src/main/java/Bookstore/Bookstore/web/BookController.java
@@ -0,0 +1,18 @@
+package Bookstore.Bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BookController {
+	
+	@GetMapping("/index")
+	@ResponseBody
+	public String indexHandler() {
+		return "Hello World";
+	}
+	
+
+}
+
