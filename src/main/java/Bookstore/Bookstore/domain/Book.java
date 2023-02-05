6cf1edbcf433259e08f07e0718fc6d3b0fccee15
Aapo <aapo.kiljunen@gmail.com>
2023-02-06 01:36:07 +0200

    tehtava 1

--- src/main/java/Bookstore/Bookstore/domain/Book.java
+++ src/main/java/Bookstore/Bookstore/domain/Book.java
@@ -1,9 +1,23 @@
 package Bookstore.Bookstore.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 
 	private String title, author, isbn;
-	private int publicationYear, price;
+	@Column(name="publication_year")
+	private int publicationYear;
+	private int price;
+
 
 	public Book(String title, String author, String isbn, int publicationYear, int price) {
 		super();
